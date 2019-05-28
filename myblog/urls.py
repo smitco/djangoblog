dc38b5ee2b4d6eacf4b5a25652a66737f1080ef8
smitco <courtjsmith11@gmail.com>
2019-05-27 19:03:39 -0700

    Lesson 07 django blog commit 2

--- myblog/urls.py
+++ myblog/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import stub_view
 
 urlpatterns = [
-    path('',
-        stub_view,
-        name="blog_index"),
+    path('', stub_view, name="blog_index"),
+    path('posts/<int:post_id>/', stub_view, name="blog_detail"),
 ]
